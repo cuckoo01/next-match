a046f5f183fa8396b8b7944316d6993c758032ca
DuyenNguyen <duyenntbse183629@fpt.edu.vn>
2025-01-13 20:07:38 +0700

    End of section 2

---
+++ src/components/Providers.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react'
+import React, { ReactNode } from 'react'
+import TopNav from './navbar/TopNav'
+
+export default function Providers({children} : {children: ReactNode}) {
+  return (
+    <NextUIProvider>
+        <TopNav />
+        <main className='container mx-auto p-10'>
+            {children}
+        </main>
+    </NextUIProvider>
+  )
+}
